6badbd6e2109177fe0b858e38d37c904fbeb6322
Becca-Mae-Hinkle <beccamae123@yahoo.com>
2022-03-13 15:01:57 -0400

    final

--- MechaCarChallenge.Rscript.R
+++ MechaCarChallenge.Rscript.R
@@ -1,13 +1,3 @@
-# Deliverable 1
-# Load the dplyr package
-library (dplyr)
-
-#Read the MechaCar_mpg.csv as a dataframe
 file <- file.choose()
-MechaCar <- read.csv(file, check.names = F,stringsAsFactors = F)
 
-#Perform the linear regression using lm()function passing all six variables with mpg as the y axis
-Mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)
 
-#Use the summary() function to determine the p-value and the r-squared value and save code to GitHub
-summary(Mecha_lm)
